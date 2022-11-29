4478cdbfb8a0ac800a29ed47812c76e70b33a70f
Abyhajon <joedabongo01@gmail.com>
2022-11-29 12:12:35 -0800

    remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
-def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
-    if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+def remove_char_at(str, n):
+    if n >= 0:
+        str = str[:n] + str[n + 1:]
+    return str 
