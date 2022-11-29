5a5d6a04e0db63e284931f73b348c35ac0bff220
Abyhajon <joedabongo01@gmail.com>
2022-11-29 21:44:46 +0300

    line break fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,5 @@ if lastDigit > 5:
 elif lastDigit == 0:
     print(f"Last digit of {number:d} is {lastDigit:d} and is 0")
 else:
-    print(f"Last digit of {number:d} is {lastDigit:d} and is less than 6 \
-        and not 0")
+    print(f"Last digit of {number:d} is {lastDigit:d} and is less than 6\
+ and not 0")
