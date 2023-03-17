0b8ddb76afa369b057c392fef18e548ef4820438
Abyhajon <joedabongo01@gmail.com>
2023-03-17 07:47:42 +0000

    hot fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const size = Number(process.argv[2]);
 
 if (isNaN(size)) {
-  console.log('Missing sizes');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     let row = '';
