5192cb214d86cdb93e1bfa586f5584d5663ff1cb
Abyhajon <joedabongo01@gmail.com>
2023-04-09 21:18:32 +0000

    task 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,23 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {}; // return an empty object
+const Rectangle = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
-}
+
+  print () {
+    let prints = '';
+    for (let cont1 = 0; cont1 < this.height; cont1++) {
+      for (let cont = 0; cont < this.width; cont++) {
+        prints = prints + 'X';
+      }
+      console.log(prints);
+      prints = '';
+    }
+  }
+};
 
 module.exports = Rectangle;
