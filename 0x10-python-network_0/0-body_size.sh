af8131c38c0fb6030596ff66a480a144d1f63ec1
Abyhajon <joedabongo01@gmail.com>
2023-04-27 10:15:03 -0700

    removed pipe to tail

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # cURL request that displays size of the response's body.
-curl -s -w '%{size_download}\n' -o /dev/null "$1" | tail -n 1
+curl -so /dev/null "$1" -w '%{size_download}\n'
