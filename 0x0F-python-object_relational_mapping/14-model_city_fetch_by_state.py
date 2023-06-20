1e2b366f94a7a0ea8dfd846faae346ff75528036
Abyhajon <joedabongo01@gmail.com>
2023-06-20 00:27:42 -0700

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
+"""
+Script that prints all City objects from the database hbtn_0e_14_usa
+"""
 
-"""module - linking databases"""
-
-from sys import argv
-from model_state import Base, State
-from model_city import City
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
+from model_state import Base, State
+from model_city import City
 
+if __name__ == '__main__':
+    # Get MySQL username, password,
+    # and database name from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-if __name__ == "__main__":
-    State.cities = relationship("City",
-                                order_by=City.id, back_populates="state")
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(argv[1],argv[2],argv[3]))
+    # Create connection URL
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name)
+
+    # Create engine and bind it to the Base class
+    engine = create_engine(url)
+    Base.metadata.create_all(engine)
+
+    # Create session
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State, City).filter(City.state_id ==
-                                              State.id).all()
-    for row in query:
-        print("{}: {} {}".format(row[0].name, row[1].id, row[1].name))
+
+    # Retrieve all City objects and sort them by cities.id
+    cities = session.query(City).order_by(City.id).all()
+
+    # Print the results
+    for city in cities:
+        state = session.query(State).filter(State.id == city.state_id).first()
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    # Close the session
     session.close()
