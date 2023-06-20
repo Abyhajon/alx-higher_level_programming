4243b0c5077b1ed55898031f7c1bca82bf66ae6a
Abyhajon <joedabongo01@gmail.com>
2023-06-19 23:34:44 -0700

    pycode style fix

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
+"""
+Script that prints the first State object from the database hbtn_0e_6_usa
+"""
 
-"""module - printing the first element"""
+import sys
 from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
+if __name__ == '__main__':
+    # Get MySQL username, password,
+    # and database name from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
+    # Create connection URL
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name)
+
+    # Create engine and bind it to the Base class
+    engine = create_engine(url)
+    Base.metadata.create_all(engine)
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Retrieve the first State object based on states.id
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Check if the table is empty
+    if first_state is None:
         print("Nothing")
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(first_state.id, first_state.name))
+
+    # Close the session
+    session.close()
