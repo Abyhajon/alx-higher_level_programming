09b0f10adbea2e486a5300633ccf9e7f94b2f0ff
Abyhajon <joedabongo01@gmail.com>
2023-07-14 06:57:11 +0000

    task 6 modified code to handle all cases

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,32 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+
+const apiUrl = process.argv[2]; // Read the API URL from the command line arguments
+
+request(apiUrl, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+
+  if (res.statusCode !== 200) {
+    console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
+    return;
+  }
+
+  // Filter the tasks for completed tasks
+  const completedTasks = body.filter((task) => task.completed);
+
+  // Compute the number of completed tasks per user ID
+  const completedTasksCount = {};
+  completedTasks.forEach((task) => {
+    const userId = task.userId;
+    completedTasksCount[userId] = completedTasksCount[userId] ? completedTasksCount[userId] + 1 : 1;
+  });
+
+  // Print the number of completed tasks per user ID
+  Object.entries(completedTasksCount).forEach(([userId, count]) => {
+    console.log(`User ID: ${userId} - Completed Tasks: ${count}`);
+  });
 });
