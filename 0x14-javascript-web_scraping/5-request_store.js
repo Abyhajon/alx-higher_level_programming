48cf82ea34ebcb26cb01e6e58b12546844b73260
Abyhajon <joedabongo01@gmail.com>
2023-07-14 07:25:00 +0000

    request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,25 @@
 #!/usr/bin/node
-const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const fs = require('fs');
+
+const url = process.argv[2]; // Read the URL from the command line arguments
+const filePath = process.argv[3]; // Read the file path from the command line arguments
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  if (res.statusCode !== 200) {
+    console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+});
